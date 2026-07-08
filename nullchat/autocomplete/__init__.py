f7dea9471efcea3fb54b02170d312f0a82490b7c
Jacob K <121004005+Jballuccii@users.noreply.github.com>
2026-07-08 13:56:51 -0400

    Add load_counts_table to __all__ exports

--- nullchat/autocomplete/__init__.py
+++ nullchat/autocomplete/__init__.py
@@ -1,11 +1,12 @@
 from nullchat.autocomplete.trie import Trie
 from nullchat.autocomplete.tst import TernarySearchTree
 from nullchat.autocomplete.engine import AutocompleteEngine
-from nullchat.autocomplete.ngrams import load_ngram_counts
+from nullchat.autocomplete.ngrams import (load_ngram_counts, load_counts_table)
 
 __all__ = [
     "Trie",
     "TernarySearchTree",
     "AutocompleteEngine",
     "load_ngram_counts",
+    "load_counts_table"
 ]
