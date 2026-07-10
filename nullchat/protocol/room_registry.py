d8872c75d697b6090807066a77f578a692e711cb
angelica tang <angelicatang07@gmail.com>
2026-07-10 13:34:57 -0400

    store multiple chats

--- nullchat/protocol/room_registry.py
+++ nullchat/protocol/room_registry.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 class RoomRegistry: # connects users to rooms
     def __init__(self):
         self._room_to_peers: dict[str, list[str]] = {}
+        self._key_to_room = {} 
+        self._rooms = set() 
 
     def add_member(self, room_id: str, peer_public_key: str) -> None:
         self._room_to_peers.setdefault(room_id, [])
@@ -14,3 +16,15 @@ class RoomRegistry: # connects users to rooms
 
     def is_known_room(self, room_id: str) -> bool:
         return room_id in self._room_to_peers
+    
+    def map_chat_key(self, chat_key, room_id):
+        self._key_to_room[chat_key] = room_id
+
+    def lookup_room_id(self, chat_key):
+        return self._key_to_room.get(chat_key)
+    
+    def add_room(self, room_id: str):
+        self._rooms.add(room_id)
+
+    def get_all_rooms(self):
+        return list(self._rooms)
